0cc255c9a1f86651bf009152f4a8a9115dea82b6
akavi <gabdushev.i@yandex.ru>
2022-07-25 17:57:20 +0500

    fix roles guard

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -2,38 +2,42 @@
 https://docs.nestjs.com/guards#guards
 */
 
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException, HttpException, HttpStatus, ForbiddenException } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+  HttpException,
+  HttpStatus,
+  ForbiddenException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
-import { Observable } from 'rxjs';
 import { UserEntity } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { ROLES_KEY } from '../decorators/roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-
-  constructor(private jwtService: JwtService,
+  constructor(
+    private jwtService: JwtService,
     private reflector: Reflector,
-    private readonly users: Repository<UserEntity>) {
-
-  }
-  async canActivate(context: ExecutionContext): Promise<boolean | Observable<boolean>> {
-
+    private readonly users: Repository<UserEntity>,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     try {
-      const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
-        context.getHandler(),
-        context.getClass()
-      ])
+      const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+        ROLES_KEY,
+        [context.getHandler(), context.getClass()],
+      );
       if (!requiredRoles) {
         return true;
       }
 
-
-      const req = context.switchToHttp().getRequest()
+      const req = context.switchToHttp().getRequest();
       const authHeader = req.headers.Authorization || req.headers.authorization;
-      const bearer = authHeader.split(' ')[0]
-      const token = authHeader.split(' ')[1]
+      const bearer = authHeader.split(' ')[0];
+      const token = authHeader.split(' ')[1];
 
       if (bearer !== 'Bearer' || !token) {
         throw new ForbiddenException('Access denied permission');
@@ -41,17 +45,18 @@ export class RolesGuard implements CanActivate {
       const user = this.jwtService.verify(token);
       req.user = user;
       const roles = await this.getRoleByEmail(req.user.email);
-      return roles.some(role => requiredRoles.includes(role));
-    }
-    catch (e) {
-      console.log(e)
+      return roles.some((role) => requiredRoles.includes(role));
+    } catch (e) {
+      console.log(e);
       throw new ForbiddenException('Access denied');
     }
-
   }
 
   async getRoleByEmail(email: string) {
-    const user = await this.users.findOne({ where: { email: email }, relations: { roles: true } })
-    return (await user).roles.map(i => i.role)
+    const user = await this.users.findOne({
+      where: { email: email },
+      relations: { roles: true },
+    });
+    return (await user).roles.map((i) => i.role);
   }
 }
